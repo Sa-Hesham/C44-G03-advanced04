19e0eb0f158875d5335f11572236b7ec8707406c
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-08-27 15:08:17 +0300

    Add project files.

---
+++ C44-G03-advanced04/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C44_G03_advanced04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
