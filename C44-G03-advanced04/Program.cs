ddd9630a77626b2fcfcff0493a3669df4bc5d377
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-08-27 16:38:47 +0300

    Q3

--- C44-G03-advanced04/Program.cs
+++ C44-G03-advanced04/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualBasic;
+using System.Diagnostics.Metrics;
 
 namespace C44_G03_advanced04
 {
@@ -37,7 +38,7 @@ namespace C44_G03_advanced04
             //    {"b",10 },
             //    {"c",20 },
             //    {"d",30 },
-                   
+
 
 
 
@@ -54,8 +55,50 @@ namespace C44_G03_advanced04
 
             //Console.WriteLine($" the key {key} has maxvalue {maxvalue}");
 
-
             #endregion
+
+
+
+
+
+            #region Q3
+            //You have a hashtable, the  user will enter targetValue find all keys that associated with a specific targetValue
+            // Note : if the targetValue not found print(“Key not found”)
+            //Dictionary<int ,string> table = new () {
+
+            //    { 1,"sayed"},
+            //    {2,"hesham" },
+            //    {3,"mostafa" },
+            //    {4,"mostafa" },
+            //    {5,"sayed" },
+
+            //};
+
+
+            //Console.WriteLine("please enter the target value to search");
+            //string inputvalue =Console.ReadLine()!.ToLower();
+            //bool flag = false;
+            //foreach (var search in table) {
+
+            //    if (search.Value == inputvalue) { 
+            //        flag = true;
+
+            //        Console.WriteLine($" {search.Key} ");
+
+            //    }
+
+
+
+            //}
+
+            //if (!flag)
+            //{
+
+            //    Console.WriteLine("key not found ");
+            //}
+            #endregion
+
+
         }
     }
 }
