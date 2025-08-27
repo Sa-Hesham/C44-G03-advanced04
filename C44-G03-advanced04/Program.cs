2785da36c5309bd5b5c722acb21e4f18ddd62946
Sa-Hesham <elsayedheshamdarwish@gmail.com>
2025-08-27 15:36:13 +0300

    Q1

--- C44-G03-advanced04/Program.cs
+++ C44-G03-advanced04/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //Given an array of integers, count the frequency of each element using a hash table.
+            //int[] Arr = { 1, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9, 1, 2, 2, 3, 9, 9 };
+            //Dictionary<int, int> freq = new Dictionary<int, int>();
+            //foreach (int i in Arr) { 
+            //if(freq.ContainsKey(i))
+            //        freq[i]++;
+            //else freq[i] = 1;
+            
+            //}
+
+            //foreach(KeyValuePair<int ,int> value in freq)
+            //{
+            //    Console.WriteLine(value );
+
+            //}
+            #endregion
         }
     }
 }
